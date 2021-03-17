4ff40330f476bf47906890c4ccd9c10dac012229
Hoang Nguyen <nguyeho2@oregonstate.edu>
2021-03-16 23:07:12 -0700

    Add carousel to home page

--- src/components/CarouselCard.js
+++ src/components/CarouselCard.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 function CarouselCard(props) {
     return (
-        <div className="col md-6">
-            <div className="card text-center">
-                <img src={props.image.src}></img>
-            </div>
+        <div className={props.image.active ? "carousel-item active text-center" : "carousel-item text-center"}>
+            <img className="h-50 w-50" src={props.image.src}></img>
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,30 +17,31 @@ function Home() {
             try {
                 let res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2012-08-06&api_key=${process.env.REACT_APP_NASA}`);
                 let body = await res.json();
-                for(let i = 0; i < 3; i++){
+                for(let i = 1; i < 5; i++){
                     newRover.push({
                         id: body.photos[i].id,
-                        src: body.photos[i].img_src
+                        src: body.photos[i].img_src,
+                        active: i == 1
                     });
                 }
 
-                res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?earth_date=2004-01-26&api_key=${process.env.REACT_APP_NASA}`);
-                body = await res.json();
-                for(let i = 0; i < 3; i++){
-                    newRover.push({
-                        id: body.photos[i].id,
-                        src: body.photos[i].img_src
-                    });
-                }
+                // res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?earth_date=2004-01-26&api_key=${process.env.REACT_APP_NASA}`);
+                // body = await res.json();
+                // for(let i = 0; i < 3; i++){
+                //     newRover.push({
+                //         id: body.photos[i].id,
+                //         src: body.photos[i].img_src
+                //     });
+                // }
 
-                res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?earth_date=2004-01-05&api_key=${process.env.REACT_APP_NASA}`);
-                body = await res.json();
-                for(let i = 0; i < 3; i++){
-                    newRover.push({
-                        id: body.photos[i].id,
-                        src: body.photos[i].img_src
-                    });
-                }
+                // res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?earth_date=2004-01-05&api_key=${process.env.REACT_APP_NASA}`);
+                // body = await res.json();
+                // for(let i = 0; i < 3; i++){
+                //     newRover.push({
+                //         id: body.photos[i].id,
+                //         src: body.photos[i].img_src
+                //     });
+                // }
             }
             catch (e) {
                 console.log("error - " + e);
@@ -56,8 +57,19 @@ function Home() {
     return (
         <div className="container">
             <h1 className="text-center">Find out what the rover has been up to!</h1>
-            <div className="row row-cols-1 row-cols-md-8">
-                {carouselCards}
+            {/* <div className="row row-cols-1 row-cols-md-8"> */}
+            <div id="carouselExampleControls" className="carousel slide bg-secondary" data-interval="false">
+                <div className="carousel-inner">
+                    {carouselCards}
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
             </div>
         </div>
     );
