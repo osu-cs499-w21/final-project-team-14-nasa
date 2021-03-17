09b0eceab9c9c65c9d55b5113ca59e5249bf01a1
Hoang Nguyen <nguyeho2@oregonstate.edu>
2021-03-16 17:05:51 -0700

    Add photo search function

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,44 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import {useParams, withRouter} from 'react-router-dom';
-// function ImageDetails(props) {
-//     return (
-//         <div>
-//             <img src={props.photo.img_src}/>
-//             <ul>
-//                 <li>
-//                     Rover: {props.photo.rover.name}
-//                 </li>
-//                 <li>
-//                     Date taken: {props.photo.earth_date}
-//                 </li>
-//                 <li>
-//                     Camera: {props.photo.camera.full_name}
-//                 </li>
-//             </ul>
-//         </div>
-//     );
-// }
+
+import PhotoCard from '../components/PhotoCard';
 
 function Search(props) {
     const {rover, date} = useParams();
     const [roverState, setRoverState] = useState("Spirit");
     const [dateState, setDateState] = useState("");
+    const [photosRes, setPhotosRes] = useState([]);
 
     useEffect(() => {
         async function searchPhotos() {
-            let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${process.env.REACT_APP_NASA}`;
-            let res = await fetch(url);
-            let body = await res.json();
-            
+            let newPhotos = [];
+            try {
+                let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${process.env.REACT_APP_NASA}`;
+                let res = await fetch(url);
+                let body = await res.json();
+                console.log(body.photos);
+                for(let i = 0; i < body.photos.length; i++){
+                    newPhotos.push({
+                        id: body.photos[i].id,
+                        src: body.photos[i].img_src,
+                        camera: body.photos[i].camera.full_name,
+                        date: body.photos[i].earth_date,
+                        rover_name: body.photos[i].rover.name
+                    })
+                }
+            }
+            catch (e) {
+                console.log("Error: " + e);
+            }
+            setPhotosRes(newPhotos);
         }
 
         if (rover != null && date != null) {
             searchPhotos();
         }
     }, [rover, date]);
-    //onSubmit={props.history.push(`/search/${roverState}/${dateState}`)}>
+
     return (
         <div className="container">
+            <h1>Search</h1>
             {
                 rover == null && date == null ? (
                     <form action={`/search/${roverState}/${dateState}`}>
@@ -60,9 +62,19 @@ function Search(props) {
                         <button type="submit" className="btn btn-primary">Search</button>
                     </form>
                 ) : (
-                    <div>
-                        <h1>Testing23</h1>
-                    </div>
+                    photosRes.length == 0 ? (
+                        <div className="row justify-content-center">
+
+                            <div className="alert alert-danger col-5" role="alert">
+                                Your search query returned no results
+                            </div>
+                        </div>
+                    ) :
+                    (
+                        <div className="row row-cols-1 row-cols-md-5">
+                            {photosRes.map((p) => <PhotoCard key={p.id} image={p}/>)}
+                        </div>
+                    )
                 )
             }
         </div>
